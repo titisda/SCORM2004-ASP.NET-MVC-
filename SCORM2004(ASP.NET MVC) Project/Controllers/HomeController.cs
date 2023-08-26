0282f60e9d398a0773d48b126d3521d1d67dcbdf
SecurityStart <136139777+SecurityStart@users.noreply.github.com>
2023-08-26 12:01:16 +0900

    Add project files.

---
+++ SCORM2004(ASP.NET MVC) Project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SCORM2004_ASP.NET_MVC__Project.Models;
+using System.Diagnostics;
+
+namespace SCORM2004_ASP.NET_MVC__Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SCORM2004(ASP.NET MVC) Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SCORM2004_ASP.NET_MVC__Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
